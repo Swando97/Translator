671e5c7beb3b95588ec68d8dd831d8c364733d4a
Swando97 <adam_swanny@hotmail.com>
2021-03-06 11:47:29 -0800

    translator app working from spanish to english

--- TranslatorApp.py
+++ TranslatorApp.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from googletrans import Translator, constants
+from pprint import pprint
+
+# init the Google API translator
+translator = Translator()
+
+# translate a spanish text to english text (by default)
+translation = translator.translate("Hola Mundo")
+print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+

8e83358491ce55e64de34f0a081c14068bcd995d
Swando97 <adam_swanny@hotmail.com>
2021-03-06 12:52:41 -0800

    Norwegian to English, few sentences changed lol

--- TranslatorApp.py
+++ TranslatorApp.py
@@ -4,17 +4,17 @@ from pprint import pprint
 # init the Google API translator
 translator = Translator()
 
-sentence = ["Dear friends and family, I am very sorry. It was never my intention to hurt anyone. Myself included.",
-"I must apologize. I want to own up to my mistakes. I was lost. I was looking outward for answers; not inward.",
-"I wasn't motivated. Having no schedule made me feel like I had no purpose. I was fighting my pain with substances.",
-"I felt more disabled than ever before. I wanted to move forward but I felt frozen. This was my fault, not yours.",
-"Now I have started to develop myself. I have a daily schedule. I see myself working towards my goals.",
-"I'm excited for the future, I hope you are too. I think about you people everyday and I cannot turn back the hands of time.",
-"I can however look back and learn from my mistakes. I realize couldn't have done this without your help and support.",
-"I will never be able to repay you guys for the things you did. I want to try. I want to start by saying I love you. Thank you."]
+sentence = ["Kjære venner og familie, jeg er veldig lei meg. Det var aldri min intensjon å skade noen. Meg inkludert.",
+"Jeg må beklage. Jeg vil eie opp til feilene mine. Jeg var tapt. Jeg så utover etter svar; ikke innover.",
+"Jeg var ikke motivert. Å ikke ha noen tidsplan fikk meg til å føle at jeg ikke hadde noen hensikt. Jeg kjempet mot smertene mine med stoffer.",
+"Jeg følte meg mer funksjonshemmet enn noen gang før. Jeg ønsket å gå videre, men jeg følte meg frossen. Dette var min feil, ikke din.",
+"Nå har jeg begynt å utvikle meg. Jeg har en daglig tidsplan. Jeg ser meg selv jobbe mot målene mine.",
+"Jeg er spent på fremtiden, jeg håper du også er det. Jeg tenker på dere mennesker hver dag, og jeg kan ikke vende tilbake hendene på tiden.",
+"Jeg kan imidlertid se tilbake og lære av feilene mine. Jeg skjønner at jeg ikke kunne ha gjort dette uten din hjelp og støtte.",
+"Jeg vil aldri være i stand til å betale tilbake dere for de tingene dere gjorde. Jeg vil prøve. Jeg vil begynne med å si at jeg elsker deg. Takk skal du ha."]
 
 # translate a spanish text to english text (by default)
-translations = translator.translate(sentence, dest="no")
+translations = translator.translate(sentence, dest="en")
 
 for translation in translations:
     print(f"{translation.text}")
